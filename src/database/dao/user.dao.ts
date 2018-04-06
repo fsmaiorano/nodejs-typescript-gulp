d39c112caf46aad6f611eaeaa83f6451d023f87d
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-04-06 08:22:28 -0300

    update: draft of database structure

---
+++ src/database/dao/user.dao.ts
@@ -0,0 +1,30 @@
+import * as mongoose from 'mongoose';
+import UserSchema from './../schemas/user.schema';
+import { User } from './../../api/models/user/user.model';
+
+class UserDAO {
+
+    private userModel = mongoose.model("users", UserSchema, "users");
+
+    constructor() {
+
+    }
+
+    save() {
+        let newUser = new this.userModel({ name: "teste" });
+        newUser.save().then((userResponse) => {
+            if (userResponse.errors) {
+                console.log('error', userResponse.errors);
+            }
+            else {
+                console.log('foi');
+            }
+        })
+    }
+}
+
+export default new UserDAO();
+
+
+
+
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,27 +1,19 @@
 import * as mongoose from 'mongoose';
 import databaseConfig from './config';
-import UserSchema from './schemas/user.schema';
-import { User } from './../api/models/user/user.model';
+
+import UserDAO from './dao/user.dao';
 
 class Database {
     constructor() {
+        this.mock();
     }
 
     initDatabase() {
         mongoose.connect(`mongodb://${databaseConfig.url}:${databaseConfig.port}/${databaseConfig.name}`);
-        const Users = mongoose.model("users", UserSchema, "users");
+    }
 
-        var newUser = new Users({ name: "teste" });
-
-        newUser.save().then((userResponse) => {
-            if (userResponse.errors) {
-                console.log('error', userResponse.errors);
-            }
-            else {
-                console.log('foi');
-            }
-        })
-
+    mock(){
+        let x = UserDAO.save();
     }
 }
 
